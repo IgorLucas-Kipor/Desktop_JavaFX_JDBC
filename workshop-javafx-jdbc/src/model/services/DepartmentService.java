02a9f20c43cc066f4a0cdf2a813b1cafbdb5ef3e
Igor Lucas <igorescritor@hotmail.com>
2021-05-30 19:05:12 -0300

    Implementando acesso a banco de dados

--- workshop-javafx-jdbc/src/model/services/DepartmentService.java
+++ workshop-javafx-jdbc/src/model/services/DepartmentService.java
@@ -3,21 +3,16 @@ package model.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
-	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 	
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<>();
-		Department department1 = new Department(1, "Books");
-		Department department2 = new Department(2, "Music");
-		Department department3 = new Department(3, "Games");
-		list.add(department1);
-		list.add(department2);
-		list.add(department3);
-		return list;
+		return dao.findAll();
 	}
 
 }

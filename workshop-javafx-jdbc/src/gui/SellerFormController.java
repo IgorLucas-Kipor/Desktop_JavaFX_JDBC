a0acf1a829e3e2c9f8a0f573b5967141049593c3
Igor Lucas <igorescritor@hotmail.com>
2021-06-01 00:15:56 -0300

    Implementando o ComboBox do Department

--- workshop-javafx-jdbc/src/gui/SellerFormController.java
+++ workshop-javafx-jdbc/src/gui/SellerFormController.java
@@ -15,19 +15,29 @@ import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
+import model.entities.Department;
 import model.entities.Seller;
 import model.exceptions.ValidationException;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerFormController implements Initializable {
+
+	private DepartmentService departmentService;
 
 	private SellerService sellerService;
 
@@ -40,25 +50,28 @@ public class SellerFormController implements Initializable {
 
 	@FXML
 	private TextField textFieldName;
-	
+
 	@FXML
 	private TextField textFieldEmail;
-	
+
 	@FXML
 	private DatePicker dpBirthDate;
-	
+
 	@FXML
 	private TextField textFieldBaseSalary;
 
 	@FXML
+	private ComboBox<Department> comboBoxDepartment;
+
+	@FXML
 	private Label errorLabel;
-	
+
 	@FXML
 	private Label errorEmail;
-	
+
 	@FXML
 	private Label errorBirthDate;
-	
+
 	@FXML
 	private Label errorBaseSalary;
 
@@ -68,12 +81,16 @@ public class SellerFormController implements Initializable {
 	@FXML
 	private Button btCancel;
 
+	@FXML
+	private ObservableList<Department> obsList;
+
 	public void setSeller(Seller entity) {
 		this.entity = entity;
 	}
 
-	public void setSellerService(SellerService service) {
-		sellerService = service;
+	public void setServices(SellerService sellerService, DepartmentService departmentService) {
+		this.sellerService = sellerService;
+		this.departmentService = departmentService;
 	}
 
 	public void subscribeDataChangeListener(DataChangeListener listener) {
@@ -96,7 +113,7 @@ public class SellerFormController implements Initializable {
 			Utils.currentStage(event).close();
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
-		} catch(ValidationException e) {
+		} catch (ValidationException e) {
 			setErrorMessages(e.getErrors());
 		}
 
@@ -141,6 +158,7 @@ public class SellerFormController implements Initializable {
 		Constraints.setTextFieldDouble(textFieldBaseSalary);
 		Constraints.setTextFieldMaxLength(textFieldEmail, 100);
 		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+		initializeComboBoxDepartment();
 	}
 
 	public void updateFormData() {
@@ -151,18 +169,45 @@ public class SellerFormController implements Initializable {
 		textFieldName.setText(entity.getName());
 		textFieldEmail.setText(entity.getEmail());
 		if (entity.getBirthDate() != null) {
-			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));			
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 		}
 		Locale.setDefault(Locale.US);
 		textFieldBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		
+		if (entity.getDepartment() == null) {
+			comboBoxDepartment.getSelectionModel().selectFirst();
+		} else {
+			comboBoxDepartment.setValue(entity.getDepartment());
+		}
 	}
-	
+
+	public void loadAssociatedObjects() {
+		if (departmentService == null) {
+			throw new IllegalStateException("Department Service is null.");
+		}
+		List<Department> list = departmentService.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		comboBoxDepartment.setItems(obsList);
+	}
+
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
-		
-		if(fields.contains("name")) {
+
+		if (fields.contains("name")) {
 			errorLabel.setText(errors.get("name"));
 		}
+	}
+
+	private void initializeComboBoxDepartment() {
+		Callback<ListView<Department>, ListCell<Department>> factory = lv -> new ListCell<Department>() {
+			@Override
+			protected void updateItem(Department item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+		comboBoxDepartment.setCellFactory(factory);
+		comboBoxDepartment.setButtonCell(factory.call(null));
 	}
 
 }
